a6edfa49ffd22fd67494cb099ceeff7ffb7bed24
Kevin <likevin@umich.edu>
2026-02-26 11:25:41 -0500

    add people profiles

--- scripts/extract_profiles.R
+++ scripts/extract_profiles.R
@@ -1,47 +1,82 @@
 library(tidyverse)
-source("scripts/make_publication.R")
+source("scripts/make_profile.R")
 
-pubspath <- "publications/publications.csv"
-pubsdir <- "publications"
+peoplepath <- "people/people.csv"
+peopledir <- "people"
 
-pubs <- read.csv(pubspath)
+people <- read.csv(peoplepath)
 
-articles <- pubs %>% filter(Item.Type == "journalArticle")
+## USE CAUTION: CLEAR CONTENTS OF people/ DIRECTORY EXCEPT FOR backup/
+current.dirs <- list.dirs(peopledir, recursive = FALSE)
+clear.dirs <- current.dirs[which(current.dirs != "people/_images")]
+unlink(clear.dirs, recursive = TRUE)
 
-for(i in seq_len(nrow(articles))){
-  title.i <- articles$Title[i]
-  author.i <- articles$Author[i] |> abbreviate_names()
-  journal.i <- articles$Publication.Title[i]
-  volume.i <- articles$Volume[i]
-  issue.i <- articles$Issue[i]
-  page.i <- articles$Pages[i]
-  year.i <- articles$Publication.Year[i]
-  url_source.i <- articles$Url[i]
-  doi.i <- articles$DOI[i]
-  # categories.i <-
+## LOOP THROUGH PEOPLE TABLE AND CREATE PEOPLE PAGES
 
-  titlewords <- strsplit(title.i," ") |> unlist()
-  file_path.i <- do.call(
-    file.path,
-    list(pubsdir,
-         paste(c(year.i,titlewords[1:3][!is.na(titlewords[1:3])]),collapse="_")
-         )
-    )
+image.index <- list.files("people/_images")
+
+for(i in seq_len(nrow(people))){
+  full_name <- c(people$First[i], people$Middle[i], people$Last[i]) |> omit.empty() |> paste(collapse=" ")
+
+  edu1 <- c(people$edu1_degree[i], people$edu1_institution[i]) |> omit.empty() |> paste(collapse=" | ")
+  edu2 <- c(people$edu2_degree[i], people$edu2_institution[i]) |> omit.empty() |> paste(collapse=" | ")
+  edu3 <- c(people$edu3_degree[i], people$edu3_institution[i]) |> omit.empty() |> paste(collapse=" | ")
+
+  edu.all <- c(edu1, edu2, edu3) |> omit.empty() |> paste(collapse = " <br> ")
+
+  file_path.i <- paste0("people/", people$Last[i],"-",people$First[i]) |> tolower()
 
   if(!dir.exists(file_path.i)){
     dir.create(file_path.i)
   }
 
-  make_publication(
-    title = title.i,
-    author = author.i,
-    journal = journal.i,
-    volume = volume.i,
-    issue = issue.i,
-    page = page.i,
-    year = year.i,
-    url_source = url_source.i,
-    doi = doi.i,
-    file_path = file.path(file_path.i,"index.qmd"),
+  person.image <- tolower(paste0(people$Last[i],"-",people$First[i],".jpg"))
+
+  if(person.image %in% image.index){
+    use.image <- person.image
+  }else{
+    use.image <- "_template.jpg"
+  }
+
+  profile.i <- list(
+    title = full_name,
+    last = people$Last[i],
+    middle = people$Middle[i],
+    first = people$First[i],
+    people_group = people$people_group[i],
+    email = people$email[i],
+    education = edu.all,
+    role = people$role[i],
+    blurb = people$blurb[i],
+    image = paste0("../_images/",use.image),
+    orcid = people$orcid[i],
+    github = people$github[i],
+    researchgate = people$researchgate[i],
+    linkedin = people$linkedin[i],
+    personal_website = people$personal_website[i],
+    file_path = file.path(file_path.i,"index.qmd")
   )
+
+  do.call(make_profile, profile.i)
+
+  cat(glue::glue(
+    '<hr>
+
+    ::: {{#about}}
+
+    {{{{< meta blurb >}}}}
+
+    ## Education
+
+    {{{{< meta education >}}}}
+
+    <br>
+    :::
+
+    ## {{{{< meta first >}}}}\'s Group Publications
+
+    ::: {{#pubs}}
+    :::', .trim = TRUE
+  ), file = file.path(file_path.i,"index.qmd"), append = TRUE)
+
 }
---
+++ scripts/make_profile.R
@@ -0,0 +1,94 @@
+library(quarto)
+source("scripts/make_publication.R")
+
+make_profile <- function(
+    title = "",
+    last = "",
+    middle = "",
+    first = "",
+    people_group = "",
+    email = "",
+    education = "",
+    role = "",
+    description = "",
+    blurb = "",
+    image = "",
+    orcid = "",
+    github = "",
+    researchgate = "",
+    linkedin = "",
+    personal_website = "",
+    file_path
+    ){
+
+  # create links
+  orcidlink <- if(orcid!="") paste0("https://orcid.org/",orcid) else ""
+  linkscontents <- c(email, orcidlink, github, researchgate, linkedin, personal_website) |>
+    setNames(c("email", "orcid", "github", "researchgate", "linkedin", "personal_website"))
+  linkscontents <- linkscontents[linkscontents != "" & !is.na(linkscontents)]   # only keep non-empty links
+  linkslist <- list()
+
+  for(z in seq_along(linkscontents)){
+    linkslist[[z]] <- makelinkinfo(names(linkscontents)[z], linkscontents[z])
+  }
+
+  yaml.args <- list(
+    title = title,
+    last = last,
+    middle = middle,
+    first = first,
+    people_group = people_group,
+    education = education,
+    subtitle = role,
+    blurb = blurb,
+    image = image,
+    toc = FALSE,
+    listing =
+      list(
+        id = "pubs",
+        template = "../../_ejs/publications.ejs",
+        contents = "../../publications/**/*.qmd",
+        sort = "pub_number desc",
+        `filter-ui` = TRUE,
+        include = list(author = abbreviate_names(paste(last,first,sep=", ")))
+      ),
+    `page-layout` = "full",
+    about =
+      list(
+        id = "about",
+        template = "trestles",
+        `image-shape` = "round",
+        image = image,
+        links = linkslist
+      )
+  )
+
+  yaml.args <- yaml.args[!is.na(yaml.args)]
+
+  pubyaml <- do.call(quarto::write_yaml_metadata_block, yaml.args)
+
+  writeLines(pubyaml, file_path)
+}
+
+omit.empty <- function(x){
+  x[which(!is.na(x) & !is.null(x) & x != "")]
+}
+
+makelinkinfo <- function(name, content){
+  icon <- switch(name,
+                 email = "envelope",
+                 orcid = "",
+                 github = "github",
+                 researchgate = "",
+                 linkedin = "linkedin",
+                 personal_website ="link")
+  text <- switch(name,
+                 email = "Email",
+                 orcid = "ORCID",
+                 github = "GitHub",
+                 researchgate = "ResearchGate",
+                 linkedin = "LinkedIn",
+                 personal_website ="Personal")
+  list(icon = icon, text = text, href = content) |> omit.empty()
+}
+
